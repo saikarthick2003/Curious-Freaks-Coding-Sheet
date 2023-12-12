7a91f42c9d7c7b811c36da5609bce06f76219eb3
saikarthick2003 <saikarthickm2020ai@gmail.com>
2023-12-12 20:59:05 +0530

    success

--- Beginners-Basic-Problems/FindOddOrEven.java
+++ Beginners-Basic-Problems/FindOddOrEven.java
@@ -1,1 +1,31 @@
+//{ Driver Code Starts
+//Initial Template for Java
+import java.io.*;
+import java.util.*;
 
+class FindOddOrEven
+{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while(t-- > 0)
+        {
+            int N = Integer.parseInt(read.readLine());
+
+            Solution ob = new Solution();
+            System.out.println(ob.oddEven(N));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+class Solution{
+    static String oddEven(int N){
+        // code here
+        if(N%2 == 0) return "even";
+        else return "odd";
+    }
+}
